efbd06ac00ce2c6d46850dcb3b61b8b3b59359d4
regularvanessaperson <vamavila4@gmail.com>
2021-04-28 23:13:48 -0500

    finalized home page and updated chatroom 1

--- components/ChatRoomCard.jsx
+++ components/ChatRoomCard.jsx
@@ -1,27 +1,18 @@
 import Link from 'next/Link'
 import styles from '../styles/Home.module.css'
 
-const ChatRoomCard = ({ value, link, onSubmit, completed }) => {
+const ChatRoomCard = ({ value, link, onSubmit, completed, count }) => {
   {
     // this card will display and show the prompt and link to the specific chatroom
     return (
      
-        <div class="relative bg-white py-6 px-6 rounded-3xl w-64 my-4 shadow-xl flex-1">
-          <div class="mt-8">
-            <p class="text-xl font-semibold my-2 grid justify-items-center flex-nowrap">{value}</p>
-            <div class="flex space-x-2 text-gray-400 text-sm grid justify-items-center">
-
-              <p>Number of people talking</p>
-            </div>
-            <div class="flex space-x-2 text-gray-400 text-sm my-3 grid justify-items-center">
-
-              <p>1 </p>
-            </div>
-            <div class="border-t-2"></div>
-
-            <div class="grid justify-items-center">
-              <Link href={link}>
-                <a >Join the conversation</a>
+        <div className="flex items-end bg-white py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+          <div className="flex items-end flex-col mt-10">
+            <p className="text-xl font-semibold flex items-start ">{value}</p>
+            <div className="border-t-2"></div>
+            <div className="flex flex-col items-end">
+              <Link href={link} >
+                <a className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Join the conversation</a>
               </Link>
             </div>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -3,46 +3,65 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import UsernameField from "../components/UsernameField";
 import ChatRoomCard from "../components/ChatRoomCard"
+import About from "../components/About"
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
   const chatSubject = "talk about mint-chocolate"
+      //State for how many users are in the chat room
+      const [count, setCount] = useState(0)
 
+      const countUsers = ()=> {
+            setCount(count+1)
+      }
+    
+      // The websocket code
+      useEffect(() => {
+        countUsers()
+        console.log(count)
+      }, []);
 
   return (
-    <div class="w-full max-w-6xl mx-auto">
+    <div className="w-full max-w-6xl mx-auto">
       {/* This sets the page's title and favicon */}
       <Head>
         <title>See who's talking!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div class="text-center max-w-xl mx-auto">
-        <h1 class="text-6xl md:text-7xl font-bold mb-5 text-gray-600">Welcome! Let's talk about icecream!</h1>
-        <h2 class="text-xl mb-5 font-light">Join your chat room and talk to people currently talking about different topics. Your name and conversation won't be stored.</h2>
+      <div className="text-center max-w-xl mx-auto ">
+        <h1 className="text-6xl md:text-7xl font-bold mb-5 text-gray-600">Welcome! <br></br> Let's talk!</h1>
+        <h2 className="text-xl mb-5 font-light">Join your chat room and talk to people currently talking about listed topics. Your name and conversation won't be stored.</h2>
       </div>
-      <div class="mx-10 md:flex flex-row  ">
+      <div className="mx-10 md:flex flex-row flex-grow">
         <ChatRoomCard
-          value={"talk about ming-chocolate"}
+          value={"talk about mint-chocolate"}
+          onClick={count}
           link={"/chatRoom1"}
-          class="flex-1"
+          className="flex-grow h-16"
         />
         <ChatRoomCard
           value={"talk about pinapple on pizza"}
           link={"/chatRoom2"}
-          class="flex-1"
+          className="flex-grow h-16"
         />
         <ChatRoomCard
           value={"talk about console vs pc"}
           link={"/chatRoom3"}
-          class="flex-1"
+          className="flex-grow h-16"
         />
           <ChatRoomCard
-          value={"talk about anything"}
-          link={"/chatRoom3"}
-          class="flex-1"
+          value={"talk about anything and everything"}
+          link={"/chatRoom4"}
+          className="flex-grow h-16"
         />
+       
       </div>
+      <div>
+         <About
+
+         />
+        </div>
     </div>
 
 
